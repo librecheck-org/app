67e421f724d9cea7649c1144e0f9e9e3662e486e
Alessio Parma <alessio.parma@gmail.com>
2023-12-20 17:07:58 +0100

    ut

--- tests/unit/helpers/index.spec.ts
+++ tests/unit/helpers/index.spec.ts
@@ -1,20 +1,37 @@
 import { afterEach, describe, expect, test, vi } from "vitest";
- import { readFromStorage } from "@/infrastructure";
+import { readFromStorage } from "@/infrastructure";
 
 describe("getCurrentUser", () => {
   afterEach(() => {
     vi.restoreAllMocks();
   });
-  
+
   test("when invoked, should read user details from storage", async () => {
     // Arrange
     vi.mock("@/infrastructure");
     const { getCurrentUser } = await import("@/helpers");
-    
+
     // Act
     await getCurrentUser();
 
     // Assert
     expect(readFromStorage).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe("newUuid", () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  test("when invoked, should return non-empty string", async () => {
+    // Arrange
+    const { newUuid } = await import("@/helpers");
+
+    // Act
+    const result = newUuid();
+
+    // Assert
+    expect(result.length).toBeGreaterThan(0);
   });
 });
