df7440eb8689856edbc3038c235fb9f7eca39de9
Alessio Parma <alessio.parma@gmail.com>
2024-01-17 12:49:55 +0100

    monaco imports

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -12,10 +12,9 @@ import { registerSW } from "virtual:pwa-register";
 
 // Monaco Editor
 import "monaco-editor/esm/vs/language/json/monaco.contribution";
+import "monaco-editor/esm/vs/language/typescript/monaco.contribution";
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
-import CssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
 import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-import HtmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import JsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
 import TsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
@@ -39,12 +38,6 @@ export function registerMonacoWorkers() {
         getWorker(_: any, label: string) {
             if (label === "json") {
                 return new JsonWorker();
-            }
-            if (label === "css" || label === "scss" || label === "less") {
-                return new CssWorker();
-            }
-            if (label === "html" || label === "handlebars" || label === "razor") {
-                return new HtmlWorker();
             }
             if (label === "typescript" || label === "javascript") {
                 return new TsWorker();
