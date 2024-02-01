ce83aa6dbee9e35ace0045c2384f3a8ff14849f0
Alessio Parma <alessio.parma@gmail.com>
2024-02-01 14:20:35 +0100

    gravatar url

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,8 +11,7 @@
                     <ion-list v-if="currentUserStore.value">
                         <ion-item>
                             <ion-avatar slot="start">
-                                <img alt="Silhouette of a person's head"
-                                    src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                                <img :alt="currentUserStore.value.fullName" :src="gravatarUrl" />
                             </ion-avatar>
                             <ion-label>
                                 <h2>{{ currentUserStore.value.fullName }}</h2>
@@ -58,9 +57,10 @@
   
 <script setup lang="ts">
 import { IonAvatar, IonButtons, IonContent, IonHeader, IonItem, IonItemDivider, IonItemGroup, IonLabel, IonList, IonMenu, IonMenuButton, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar } from "@ionic/vue";
-import SystemStatusMenuItems from "@/components/SystemStatusMenuItems.vue";
 import { onMounted, ref } from "vue";
+import SystemStatusMenuItems from "@/components/SystemStatusMenuItems.vue";
 import router from "@/router";
+import { sha256 } from "@/helpers";
 import { useCurrentUserStore } from "@/stores";
 import { useHomeViewModel } from "@/viewModels";
 
@@ -69,7 +69,15 @@ const mainMenu = ref<InstanceType<typeof IonMenu> | null>(null);
 const { commands } = useHomeViewModel();
 const currentUserStore = useCurrentUserStore();
 
-onMounted(async () => await currentUserStore.refresh());
+const gravatarUrl = ref("https://gravatar.com/avatar/");
+onMounted(async () => {
+    await currentUserStore.refresh();
+
+    const emailAddressHash = await sha256(currentUserStore.value!.emailAddress);
+    console.log("hash", emailAddressHash);
+    gravatarUrl.value += `${emailAddressHash}`;
+});
+
 
 async function closeMainMenu() {
     if (mainMenu.value !== null) {
